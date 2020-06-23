e0e01054f329a5b9ebe79f73c7d11f5b2fbec6d5
илья кстров <ilya.ilya-kostrov2015@yandex.ru>
2020-06-23 15:28:29 +0300

    1.2

--- Usunik.Designer.cs
+++ Usunik.Designer.cs
@@ -28,12 +28,204 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBoxMesto = new System.Windows.Forms.TextBox();
+            this.textBoxTime = new System.Windows.Forms.TextBox();
+            this.textBoxNAPRAVLENIE = new System.Windows.Forms.TextBox();
+            this.textBoxDIS = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listViewPREPOD = new System.Windows.Forms.ListView();
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader9 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader10 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.buttonDel = new System.Windows.Forms.Button();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::KVANTORIUM.Properties.Resources.dfsfsdsdf;
+            this.pictureBox1.Location = new System.Drawing.Point(462, 59);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(157, 166);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 60;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBoxMesto
+            // 
+            this.textBoxMesto.Location = new System.Drawing.Point(301, 217);
+            this.textBoxMesto.Name = "textBoxMesto";
+            this.textBoxMesto.Size = new System.Drawing.Size(136, 20);
+            this.textBoxMesto.TabIndex = 59;
+            // 
+            // textBoxTime
+            // 
+            this.textBoxTime.Location = new System.Drawing.Point(301, 175);
+            this.textBoxTime.Name = "textBoxTime";
+            this.textBoxTime.Size = new System.Drawing.Size(136, 20);
+            this.textBoxTime.TabIndex = 58;
+            // 
+            // textBoxNAPRAVLENIE
+            // 
+            this.textBoxNAPRAVLENIE.Location = new System.Drawing.Point(301, 127);
+            this.textBoxNAPRAVLENIE.Name = "textBoxNAPRAVLENIE";
+            this.textBoxNAPRAVLENIE.Size = new System.Drawing.Size(136, 20);
+            this.textBoxNAPRAVLENIE.TabIndex = 57;
+            // 
+            // textBoxDIS
+            // 
+            this.textBoxDIS.Location = new System.Drawing.Point(301, 88);
+            this.textBoxDIS.Name = "textBoxDIS";
+            this.textBoxDIS.Size = new System.Drawing.Size(136, 20);
+            this.textBoxDIS.TabIndex = 56;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(298, 198);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(116, 13);
+            this.label4.TabIndex = 55;
+            this.label4.Text = "Место расположение";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(298, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 54;
+            this.label3.Text = "Время";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(298, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 53;
+            this.label2.Text = "Направление";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(298, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 52;
+            this.label1.Text = "Дисциплина";
+            // 
+            // listViewPREPOD
+            // 
+            this.listViewPREPOD.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader7,
+            this.columnHeader8,
+            this.columnHeader9,
+            this.columnHeader10});
+            this.listViewPREPOD.FullRowSelect = true;
+            this.listViewPREPOD.GridLines = true;
+            this.listViewPREPOD.HideSelection = false;
+            this.listViewPREPOD.Location = new System.Drawing.Point(12, 16);
+            this.listViewPREPOD.MultiSelect = false;
+            this.listViewPREPOD.Name = "listViewPREPOD";
+            this.listViewPREPOD.Size = new System.Drawing.Size(280, 264);
+            this.listViewPREPOD.TabIndex = 51;
+            this.listViewPREPOD.UseCompatibleStateImageBehavior = false;
+            this.listViewPREPOD.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "Дисциплина";
+            this.columnHeader7.Width = 78;
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "Направление";
+            this.columnHeader8.Width = 88;
+            // 
+            // columnHeader9
+            // 
+            this.columnHeader9.Text = "Время";
+            // 
+            // columnHeader10
+            // 
+            this.columnHeader10.Text = "Место";
+            // 
+            // buttonDel
+            // 
+            this.buttonDel.Location = new System.Drawing.Point(452, 267);
+            this.buttonDel.Name = "buttonDel";
+            this.buttonDel.Size = new System.Drawing.Size(94, 23);
+            this.buttonDel.TabIndex = 50;
+            this.buttonDel.Text = "удалить";
+            this.buttonDel.UseVisualStyleBackColor = true;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(301, 261);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(94, 29);
+            this.buttonAdd.TabIndex = 49;
+            this.buttonAdd.Text = "создать";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            // 
+            // Usunik
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(631, 306);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBoxMesto);
+            this.Controls.Add(this.textBoxTime);
+            this.Controls.Add(this.textBoxNAPRAVLENIE);
+            this.Controls.Add(this.textBoxDIS);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listViewPREPOD);
+            this.Controls.Add(this.buttonDel);
+            this.Controls.Add(this.buttonAdd);
+            this.Name = "Usunik";
             this.Text = "Usunik";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBoxMesto;
+        private System.Windows.Forms.TextBox textBoxTime;
+        private System.Windows.Forms.TextBox textBoxNAPRAVLENIE;
+        private System.Windows.Forms.TextBox textBoxDIS;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listViewPREPOD;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.ColumnHeader columnHeader8;
+        private System.Windows.Forms.ColumnHeader columnHeader9;
+        private System.Windows.Forms.ColumnHeader columnHeader10;
+        private System.Windows.Forms.Button buttonDel;
+        private System.Windows.Forms.Button buttonAdd;
     }
 }
